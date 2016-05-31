1eebe018b4d3923f52190499ea3837f72b847a07
Ricky Freyre <ricky.freyre@gmail.com>
2016-05-30 23:54:22 -0400

    Release

--- FYData2EA/My Project/AssemblyInfo.vb
+++ FYData2EA/My Project/AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("Copyright ©  2016")>
 <Assembly: AssemblyTrademark("")>
 
-<Assembly: ComVisible(False)>
+<Assembly: ComVisible(True)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("d8332c52-45e8-4a11-9e33-6ef80c5f8926")>
